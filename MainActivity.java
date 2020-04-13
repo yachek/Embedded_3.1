8811cd9161f57fe666a309b233575185e0f92cf0
yachek <yachek008@gmail.com>
2020-04-13 13:51:01 +0300

    Some improvements

--- MainActivity.java
+++ MainActivity.java
@@ -36,11 +36,11 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 String tmp = num.getText().toString();
                 BigInteger number = new BigInteger(tmp);
-                if (number.mod(BigInteger.valueOf(2)).equals(BigInteger.valueOf(0)) || tmp == "") {
+                if (number.mod(BigInteger.valueOf(2)).equals(BigInteger.ZERO)) {
                     Toast toast = Toast.makeText(getApplicationContext(),
                             "Введіть непарне число!", Toast.LENGTH_SHORT);
                     toast.show();
-                } else if (isPrime(number)) {
+                } else if (number.isProbablePrime(1) || number.equals(BigInteger.ONE)) {
                     String temp = number.longValue() + " просте!";
                     result.setText(temp);
                 } else {
@@ -52,50 +52,56 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void ferma(BigInteger num) {
-        long s = (long)Math.sqrt(num.longValue());
-        long y;
+        BigInteger s = sqrtInt(num);
+        BigInteger y;
         double radical;
-        for (int k = 0; k < num.intValue(); k++) {
-            y = (long)Math.pow(s + k, 2) - num.longValue();
-            radical = Math.sqrt(y);
+        BigInteger k = BigInteger.ZERO;
+        for (;; k = k.add(BigInteger.ONE)) {
+            y = s.add(k).pow(2).subtract(num);
+            radical = Math.sqrt(y.doubleValue());
             if (radical % 1 == 0.0) {
-                BigInteger mul1 = BigInteger.valueOf((long)(s + k + radical));
-                BigInteger mul2 = BigInteger.valueOf((long)(s + k - radical));
-                if (isPrime(mul1)) {
+                BigInteger mul1 = s.add(k).add(BigInteger.valueOf((long)radical));
+                BigInteger mul2 = s.add(k).subtract(BigInteger.valueOf((long)radical));
+                if (mul1.isProbablePrime(1)) {
                     res.add(mul1);
                 } else {
                     ferma(mul1);
                 }
-                if (isPrime(mul2)) {
+                if (mul2.isProbablePrime(1)) {
                     res.add(mul2);
                 } else {
                     ferma(mul2);
                 }
                 break;
             }
+            if (k.compareTo(num) > -1) break;
         }
     }
 
     private void out() {
         StringBuilder temp = new StringBuilder();
         for (int i = 0; i < res.size()-1; i++) {
-            String tmp = res.get(i).longValue() + " * ";
+            String tmp = res.get(i).toString() + " * ";
             temp.append(tmp);
         }
-        temp.append(res.get(res.size()-1).longValue());
+        temp.append(res.get(res.size()-1).toString());
         temp.append(" = ");
         temp.append(num.getText().toString());
         result.setText(temp);
         res.clear();
     }
 
-    private boolean isPrime(BigInteger n) {
-        for (int i = 2; i < n.intValue(); i++) {
-            if (n.mod(BigInteger.valueOf(i)).equals(BigInteger.valueOf(0))) {
-                return false;
-            }
+    public static BigInteger sqrtInt(BigInteger n) {
+        BigInteger a = BigInteger.ONE;
+        BigInteger b = n.shiftRight(1).add(new BigInteger("2"));
+        while (b.compareTo(a) >= 0) {
+            BigInteger mid = a.add(b).shiftRight(1); // (a+b) >> 1
+            if (mid.multiply(mid).compareTo(n) > 0)
+                b = mid.subtract(BigInteger.ONE);
+            else
+                a = mid.add(BigInteger.ONE);
         }
-        return true;
+        return a.subtract(BigInteger.ONE);
     }
 
 }
